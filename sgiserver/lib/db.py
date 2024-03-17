1ff34cd55c8d6f917a86729fe8828ce63ef182b9
valera-md <150794469+valera-md@users.noreply.github.com>
2024-03-17 19:26:23 +0200

    Update db.py

--- sgiserver/lib/db.py
+++ sgiserver/lib/db.py
@@ -1,6 +1,6 @@
 import psycopg2
 
 def connect():
- conn = psycopg2.connect("dbname=chat_db user=postgres password=qazwsx host=localhost port=5889")
+ conn = psycopg2.connect("dbname=chat_db user=postgres password=password host=localhost port=5889")
  curs = conn.cursor()
  return conn, curs
